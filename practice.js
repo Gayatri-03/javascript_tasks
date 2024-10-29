a93e3651a8ae3ea60707fd5a29628bcf9a0ba96f
Gayatri-03 <gayatriannareddy@gmail.com>
2024-10-29 10:49:53 +0530

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,8 @@
+const example = new Array("ex1" , "ex2", 45 , true);
+console.log(example);
+example[2]=68;
+console.log(example);
+example[4]=90;
+console.log(example);
+example.pop();
+console.log(example);
